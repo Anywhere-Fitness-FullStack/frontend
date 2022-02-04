3c7ad3ea9783fcff3a4c2e1b9866ef54691475e1
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-02-04 16:42:10 -0500

    finalized project.

--- src/components/AddClass/AddClassForm.js
+++ src/components/AddClass/AddClassForm.js
@@ -1,122 +1,127 @@
-import React from "react";
-import { Button, Form, Input, Label} from 'reactstrap';
+import React from 'react';
+import { Button, Form, Input, Label } from 'reactstrap';
 
 const AddClassForm = (props) => {
+	const { values, submit, change, disabled, errors } = props;
 
-    const { values, submit, change, disabled, errors } = props
-    
+	const onSubmit = (evt) => {
+		evt.preventDefault();
+		submit();
+	};
 
-    const onSubmit = evt => {
-        evt.preventDefault()
-        submit()
-    }
+	const onChange = (evt) => {
+		const { name, value } = evt.target;
 
-    const onChange = evt => {
-        
-        const { name, value } = evt.target;
-        
-        change(name, value);
-      }
+		change(name, value);
+	};
 
+	return (
+		<Form onSubmit={onSubmit}>
+			<h2>ADD Class</h2>
 
-    return (
-        <Form onSubmit={onSubmit}>
-            <h2>ADD Class</h2>
+			<div>
+				<div>{errors.class_name}</div>
+				<div>{errors.class_type}</div>
+				<div>{errors.dropdown}</div>
+				<div>{errors.start}</div>
+			</div>
+			<Label>
+				Class Name:
+				<Input
+					name="class_name"
+					type="text"
+					value={values.class_name}
+					onChange={onChange}
+				/>
+			</Label>
+			<br />
+			<Label>
+				Class Type
+				<Input
+					type="text"
+					name="class_type"
+					value={values.class_type}
+					onChange={onChange}
+				/>
+			</Label>
+			<br />
+			<Label>
+				Start Time
+				<Input
+					type="time"
+					name="start_time"
+					value={values.start_time}
+					onChange={onChange}
+				/>
+			</Label>
+			<br />
+			<Label>
+				Duration
+				<Input
+					name="duration"
+					type="number"
+					min="0"
+					placeholder="minutes"
+					value={values.duration}
+					onChange={onChange}
+				/>
+			</Label>
+			<br />
+			<Label>
+				Intensity Level
+				<select
+					name="intensity_level"
+					value={values.intensity_level}
+					onChange={onChange}
+				>
+					<option>-Select Intensity-</option>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+				</select>
+			</Label>
+			<br />
+			<Label>
+				Location
+				<Input
+					type="text"
+					name="location"
+					value={values.location}
+					onChange={onChange}
+				/>
+			</Label>
+			{/* <br />
+			<Label>
+				Current Clients
+				<Input
+					name="current_clients"
+					type="number"
+					min="0"
+					value={values.current_clients}
+					onChange={onChange}
+				/>
+			</Label> */}
+			<br />
+			<Label>
+				Max Class Size
+				<Input
+					type="number"
+					min="0"
+					name="max_class_size"
+					value={values.max_class_size}
+					onChange={onChange}
+				/>
+			</Label>
+			<br />
 
-            <div>
-          <div>{errors.class_name}</div>
-          <div>{errors.class_type}</div>
-          <div>{errors.dropdown}</div>
-          <div>{errors.start}</div>
-        </div>
-            <Label>Class Name:
-                <Input 
-                    name="class_name"
-                    type="text"
-                    value={values.class_name}
-                    onChange={onChange}
-                />
-                
-            </Label>
-            <br />
-            <Label>Class Type
-                <Input 
-                type="text"
-                name="class_type"
-                value={values.class_type}
-                onChange={onChange}
-                 />
-            </Label>
-                <br />
-            <Label>Start Time
-                <Input 
-                type="time" 
-                name="start" 
-                value={values.start}
-                onChange={onChange}
-                />
-            </Label>
-                <br />
-            <Label>Duration
-                <Input 
-                name="duration" 
-                type="number" 
-                min="0" 
-                placeholder="minutes" 
-                value={values.duration}
-                onChange={onChange}
-                />
-            </Label>
-                <br />
-            <Label>Intensity Level
-                <select 
-                name="dropdown"
-                value={values.dropdown}
-                onChange={onChange}
-                 >
-                <option>-Select Intensity-</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                </select>
-            </Label>
-                <br/>
-            <Label>Location
-                <Input 
-                type="text" 
-                name="location"  
-                value={values.location}
-                onChange={onChange}
-                />
-            </Label>
-                <br/>
-            <Label>Current Clients
-                <Input 
-                name="currentClients"
-                type="number" 
-                min="0" 
-                value={values.currentClients}
-                onChange={onChange}
-                />
-            </Label>
-                <br />
-            <Label>Max Class Size
-                <Input 
-                type="number" 
-                min="0" 
-                name="maxSize"
-                value={values.maxSize}
-                onChange={onChange}
-                />
-            </Label>
-                <br />
-              
-                    {/* ERRORS */}
-                <Button disabled={disabled} type="submit">Create Class</Button>
-        </Form>
-    )
-}
+			{/* ERRORS */}
+			<Button disabled={disabled} type="submit">
+				Create Class
+			</Button>
+		</Form>
+	);
+};
 
 export default AddClassForm;
--- src/validations/FormSchema.js
+++ src/validations/FormSchema.js
@@ -1,20 +1,21 @@
-import * as yup from "yup";
+import * as yup from 'yup';
 
 const addSchema = yup.object().shape({
-  class_name: yup
-    .string()
-    .required("Class Name is Required!")
-    .min(3, "Name must be at least 3 characters"),
-  class_type: yup
-    .string()
-    .required("Class type is Required!")
-    .min(3, "Type must be at least 3 characters"),
-  start_time: yup.string().required(),
-  duration: yup.string().required(),
-  intensity_level: yup.string().required(),
-  location: yup.string().required(),
-  max_class_size: yup.string().required(),
-  current_clients: yup.string().required(),
+	class_name: yup
+		.string()
+		.required('Class Name is Required!')
+		.min(3, 'Name must be at least 3 characters'),
+	class_type: yup
+		.string()
+		.required('Class type is Required!')
+		.min(3, 'Type must be at least 3 characters'),
+	start_time: yup.string().required(),
+	duration: yup.string().required(),
+	intensity_level: yup.string().required(),
+	location: yup.string().required(),
+	max_class_size: yup.string().required(),
+	// current_clients: yup.string().required(),
+	// instructor_id: yup.string().required(),
 });
 
 export default addSchema;
